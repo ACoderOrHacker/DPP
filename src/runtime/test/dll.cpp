87d617315ea9c620206c5f695b2d4756719ed378
ACoder <sgy2788@163.com>
2024-08-04 12:24:49 +0800

    update .gitignore

--- src/runtime/test/dll.cpp
+++
@@ -1,8 +0,0 @@
-// this is a dll sample
-
-#include "rt_test.hpp"
-#include <iostream>
-
-extern "C" __declspec(dllexport) Dpp_Object *__add(FObject *) {
-	return mkConst<IntObject, Interger>(INT_TYPE, 1 + 2);
-}
