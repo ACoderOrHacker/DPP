50b07e4fb928897f535825a1dad45aa0e2f9dfee
ACoderOrHacker <121915838+ACoderOrHacker@users.noreply.github.com>
2025-01-26 20:01:34 +0800

    Update dnf.sh

--- scripts/dnf.sh
+++ scripts/dnf.sh
@@ -4,58 +4,22 @@ sudo cat > /etc/yum.repos.d/fedora.repo << EOF
 [fedora]
 name=Fedora-TUNA
 failovermethod=priority
-baseurl=https://mirrors.tuna.tsinghua.edu.cn/fedora/releases/39/Everything/\$basearch/os/
+baseurl=https://mirrors.tuna.tsinghua.edu.cn/fedora/releases/41/Everything/\$basearch/os/
 enabled=1
 metadata_expire=28d
 gpgcheck=0
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-39-\$basearch
-
-[fedora-debuginfo]
-name=Fedora-TUNA-Debug
-failovermethod=priority
-baseurl=https://mirrors.tuna.tsinghua.edu.cn/fedora/releases/39/Everything/\$basearch/debug/tree/
-enabled=0
-metadata_expire=28d
-gpgcheck=0
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-39-\$basearch
-
-[fedora-source]
-name=Fedora
-failovermethod=priority
-baseurl=https://mirrors.tuna.tsinghua.edu.cn/fedora/releases/39/Everything/source/tree/
-enabled=0
-metadata_expire=28d
-gpgcheck=0
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-39-\$basearch
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-41-\$basearch
 EOF
 
 sudo cat > /etc/yum.repos.d/fedora-updates.repo << EOF
 [updates]
 name=Fedora Updates - TUNA
 failovermethod=priority
-baseurl=https://mirrors.tuna.tsinghua.edu.cn/fedora/updates/39/Everything/\$basearch/
+baseurl=https://mirrors.tuna.tsinghua.edu.cn/fedora/updates/41/Everything/\$basearch/
 enabled=1
 metadata_expire=6h
 gpgcheck=0
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-39-\$basearch
-
-[updates-debuginfo]
-name=Fedora - Updates - Debug - TUNA
-failovermethod=priority
-baseurl=https://mirrors.tuna.tsinghua.edu.cn/fedora/updates/39/Everything/\$basearch/debug/tree/
-enabled=0
-metadata_expire=6h
-gpgcheck=0
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-39-\$basearch
-
-[updates-source]
-name=Fedora - Updates - Source - TUNA
-failovermethod=priority
-baseurl=https://mirrors.tuna.tsinghua.edu.cn/fedora/updates/39/Everything/source/tree/
-enabled=0
-metadata_expire=6h
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-39-\$basearch
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-41-\$basearch
 EOF
 
 sudo chmod 644 /etc/yum.repos.d/fedora.repo
